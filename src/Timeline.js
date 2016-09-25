d0699c0a7d118238b0edd78586da37025af33554
Nícolas Iensen <nicolas.iensen@gmail.com>
2016-09-25 14:30:13 +0200

    Add the Timeline component

---
+++ src/Timeline.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+import { loadTweets } from './api'
+import Tweet from './Tweet'
+
+class Timeline extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {tweets: [], loading: false}
+  }
+
+  componentWillMount () {
+    this.setState({loading: true})
+    loadTweets(this.props.accessToken.token, this.props.accessToken.secret).end(
+      (err, res) => {
+        this.setState({tweets: res.body, loading: false})
+      }
+    )
+  }
+
+  render () {
+    return (
+      this.state.loading
+      ? <div>loading...</div>
+      : (
+        <div>
+          {this.state.tweets.map(t => <Tweet key={t.id} text={t.text} />)}
+        </div>
+      )
+    )
+  }
+}
+
+export default Timeline
