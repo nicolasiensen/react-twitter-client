d7bf0cfd576303aca6ae70ac8fd6242134d7afe2
Nícolas Iensen <nicolas.iensen@gmail.com>
2016-09-30 08:30:31 +0200

    Avoid loading the request token upfront in the Login component, which was breaking the Chrome extension flow. Now the request token is loaded only when the user clicks on the authorize button

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,6 +9,7 @@ class Login extends Component {
 
     this.changePincode = this.changePincode.bind(this)
     this.submitPincode = this.submitPincode.bind(this)
+    this.redirectToAuthorizeUrl = this.redirectToAuthorizeUrl.bind(this)
 
     this.state = {
       loadingRequestToken: false,
@@ -16,13 +17,14 @@ class Login extends Component {
     }
   }
 
-  componentWillMount() {
+  redirectToAuthorizeUrl () {
     this.setState({loadingRequestToken: true})
 
     loadRequestToken().end(
       (err, res) => {
         storage.setItem('requestToken', res.body)
         this.setState({loadingRequestToken: false})
+        window.open(res.body.authorize_url)
       }
     )
   }
@@ -41,25 +43,25 @@ class Login extends Component {
 
   render() {
     return (
-      this.state.loadingRequestToken
-      ? <div>Loading...</div>
-      : (
-        <div>
-          <p>
-            Welcome! Click on the button bellow to authorize the APP to access your Twitter account.
-          </p>
-          <p>
-            <a href={storage.getItem('requestToken').authorize_url} target='_blank'>Authorize</a>
-          </p>
-          <p>
-            After authorizing the APP you can come back here to enter your pincode:
-          </p>
-          <p>
-            <input placeholder='Pincode' value={this.state.pincode} onChange={this.changePincode} />
-            <button onClick={this.submitPincode}>Submit pincode</button>
-          </p>
-        </div>
-      )
+      <div>
+        <p>
+          Welcome! Click on the button bellow to authorize the APP to access your Twitter account.
+        </p>
+        <p>
+          <button
+            disabled={this.state.loadingRequestToken}
+            onClick={this.redirectToAuthorizeUrl}>
+            {this.state.loadingRequestToken ? 'Loading...' : 'Authorize'}
+          </button>
+        </p>
+        <p>
+          After authorizing the APP you can come back here to enter your pincode:
+        </p>
+        <p>
+          <input placeholder='Pincode' value={this.state.pincode} onChange={this.changePincode} />
+          <button onClick={this.submitPincode}>Submit pincode</button>
+        </p>
+      </div>
     )
   }
 }
