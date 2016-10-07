2966567baf664422d8211d148c658e2468a84b44
Nícolas Iensen <nicolas.iensen@gmail.com>
2016-10-07 07:58:03 +0200

    Add styles variables

---
+++ src/lib/styles.js
@@ -0,0 +1,7 @@
+export const space1 = '0.5rem'
+export const space2 = '1rem'
+export const space3 = '2rem'
+export const space4 = '4rem'
+
+export const lightGray = '#eee'
+export const white = '#fff'
