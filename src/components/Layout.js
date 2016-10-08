0eb3a263a27638e3b728de5c490961eb3a195768
Nícolas Iensen <nicolas.iensen@gmail.com>
2016-10-08 08:46:00 +0200

    Add Layout component to wrap the layout style

---
+++ src/components/Layout.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import Radium from 'radium'
+
+import { space2, lightGray, smBreakpoint } from './../lib/styles'
+
+const styles = {
+  container: {
+    background: lightGray,
+    minHeight: '100%',
+    [`@media (min-width: ${smBreakpoint})`]: {
+      padding: space2
+    }
+  },
+  content: {
+    maxWidth: '900px',
+    margin: '0 auto'
+  }
+}
+
+class Layout extends Component {
+  render() {
+    return (
+      <div style={styles.container}>
+        <div style={styles.content}>
+          {this.props.children}
+        </div>
+      </div>
+    )
+  }
+}
+
+Layout.propTypes = {
+  children: React.PropTypes.object
+}
+
+export default Radium(Layout)
