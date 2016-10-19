8de6258ca3f4f8c29007eecb1cbc4be165886b65
Nícolas Iensen <nicolas.iensen@gmail.com>
2016-10-19 08:03:37 +0200

    Add more style variables

--- src/lib/styles.js
+++ src/lib/styles.js
@@ -3,8 +3,23 @@ export const space2 = '1rem'
 export const space3 = '2rem'
 export const space4 = '4rem'
 
+export const h1 = '2rem'
+export const h2 = '1.5rem'
+export const h3 = '1.25rem'
+export const h4 = '1rem'
+export const h5 = '.875rem'
+export const h6 = '.75rem'
+
 export const lightGray = '#eee'
+export const gray = '#ccc'
 export const white = '#fff'
+export const blue = '#1da1f2'
+export const primaryColor = blue
+export const disabledColor = gray
+
+export const borderRadius = '0.2rem'
+export const fontBold = 'bold'
+export const inputBorderWidth = '1px'
 
 // https://material.google.com/layout/responsive-ui.html
 export const xsBreakpoint = '480px'
