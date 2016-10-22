79e164811aea1907db5c48ed2cc26a6ad8159989
Nícolas Iensen <nicolas.iensen@gmail.com>
2016-10-22 10:51:15 +0200

    Remove unused lib from Login.test.js

--- tests/components/Login.test.js
+++ tests/components/Login.test.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import ReactTestUtils from 'react-addons-test-utils'
 
 import Login from './../../src/components/Login'
 import storage from './../../src/lib/storage'
